cfa71d2a3c64a467bf41e72793238a59288db994
刘景平 <liujingping@idwzx.com>
2020-10-15 17:29:37 +0800

    @Create注解的缓存实例新增初始化方式

--- jetcache/jetcache-provider/src/main/java/com/fullmoon/study/service/impl/ChildServiceImpl.java
+++ jetcache/jetcache-provider/src/main/java/com/fullmoon/study/service/impl/ChildServiceImpl.java
@@ -2,13 +2,17 @@ package com.fullmoon.study.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.alicp.jetcache.Cache;
+import com.alicp.jetcache.RefreshPolicy;
 import com.alicp.jetcache.anno.*;
+import com.alicp.jetcache.embedded.CaffeineCacheBuilder;
 import com.fullmoon.study.entity.Child;
 import com.fullmoon.study.entity.People;
 import com.fullmoon.study.service.ChildService;
 import org.springframework.util.CollectionUtils;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service(
         version = "${study.service.version}",
@@ -21,7 +25,26 @@ public class ChildServiceImpl implements ChildService {
 
     @CreateCache(name = "childCache" , localLimit = 50 ,cacheType = CacheType.LOCAL)
     @CachePenetrationProtect
-    Cache<String, Child> childCache;
+    public Cache<String, Child> childCache;
+
+    private Cache<Integer, String> exampleCache;
+
+    @PostConstruct
+    public void init(){
+        RefreshPolicy policy = RefreshPolicy.newPolicy(60, TimeUnit.MINUTES)
+                .stopRefreshAfterLastAccess(120, TimeUnit.MINUTES);
+        childCache.config().setLoader(name -> {
+            Child child = new Child();
+            child.setName(name);
+            return child;
+        });
+        childCache.config().setRefreshPolicy(policy);
+
+        exampleCache = CaffeineCacheBuilder.createCaffeineCacheBuilder()
+                .limit(10)
+                .expireAfterWrite(60, TimeUnit.SECONDS)
+                .buildCache();
+    }
 
     @Override
     @Cached(name = "ChildService.getChildren", expire = 300, localLimit = 50, cacheType = CacheType.BOTH)
