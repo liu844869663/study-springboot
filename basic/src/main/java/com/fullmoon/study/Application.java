f32fad00e2a4e2bde241bb18c09d7a3483e88f42
刘景平 <liujingping@idwzx.com>
2020-11-19 22:48:37 +0800

    启动类上面添加 @EnableTransactionManagement 注解，开启事务的支持

--- basic/src/main/java/com/fullmoon/study/Application.java
+++ basic/src/main/java/com/fullmoon/study/Application.java
@@ -3,8 +3,10 @@ package com.fullmoon.study;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 @MapperScan("com.fullmoon.study.dao")
 public class Application {
     public static void main(String[] args){
